1a8c5a25553e9547a772e0b5ac016274ce454e37
giagra91 <giagra91@gmail.com>
2022-02-25 17:19:47 +0100

    modifche JS per output mail utente

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
 const acceptedMail = ["topogigio@gmail.com", "pincopallino@gmail.com", "boolean@gmail.com", "classe55@gmail.com", "peakyblinders@netflix.com"];
 const userMail = document.getElementById(`user-mail`);
+let userOutput = document.getElementById("mail-output");
 
+let emailFound = false;
 
 document.getElementById("check-mail").addEventListener("click", function(){
 
+    // userMail.value="";
+    let newMail = userMail.value.toLowerCase();
     for (i = 0; i < acceptedMail.length; i++){
         
-        let newMail = userMail.value;
-        console.log(newMail);
-        // console.log(acceptedMail[i]);
     
-        // if (newMail === acceptedMail[i]){
-        //     console.log("Puoi accedere");
-        // } else if (newMail !== acceptedMail[i]) {
-        //     console.log("Non puoi accedere");
+        if (newMail === acceptedMail[i]){
+            console.log("Puoi accedere");
+            emailFound = true;
+        }
+        
+    }
 
-        // }
+    if (emailFound === true){
+        emailFound = true;
+        userOutput.innerHTML= "Puoi accedere";
+    } else {
+        emailFound = false;
+        userOutput.innerHTML= "Non puoi accedere";
     }
 })
